c519751c3de5a9b024afeaaacf0749562498c4fa
mufeez123-lab <mufizmalar@gmail.com>
2026-06-24 12:11:10 +0530

    updated

--- promoter/login.php
+++ promoter/login.php
@@ -155,7 +155,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         .login-header {
-            background: linear-gradient(180deg, var(--secondary-color) 0%, var(--primary-color) 100%);
+            background: linear-gradient(360deg, var(--secondary-color) 0%, var(--primary-color) 100%);
             color: var(--light-text);
             padding: 30px;
             text-align: center;
