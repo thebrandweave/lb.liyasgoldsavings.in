35f65b957630afbc4bc30145659a009db4c32d94
mufeez123-lab <mufizmalar@gmail.com>
2026-06-24 11:58:30 +0530

    updated

--- promoter/topten/index.php
+++ promoter/topten/index.php
@@ -522,14 +522,14 @@ $remainingEarners = array_slice($topEarners, 3);
                     angle: 60,
                     spread: 55,
                     origin: { x: 0, y: 0.85 },
-                    colors: ['#000000', '#eab308', '#ffffff']
+                    colors: ['#000000', '#eab308', '#ff0000']
                 });
                 confetti({
                     particleCount: 3,
                     angle: 120,
                     spread: 55,
                     origin: { x: 1, y: 0.85 },
-                    colors: ['#000000', '#eab308', '#ffffff']
+                    colors: ['#000000', '#eab308', '#8a8a8a']
                 });
 
                 if (Date.now() < end) {
