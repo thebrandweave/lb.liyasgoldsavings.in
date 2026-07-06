e3b0dfafc4c9087b5331d24803116d02d44835d6
mufeez123-lab <mufizmalar@gmail.com>
2026-07-06 16:01:00 +0530

    updated

--- customer/schemes/index.php
+++ customer/schemes/index.php
@@ -284,7 +284,7 @@ $activeSubscriptions = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
         .table thead th {
-            background:linear-gradient(180deg, #ffbb50, #000000);
+            background-color: #684e26;
             color: var(--text-primary);
             font-weight: 500;
             border-bottom: none;
